bbef6fd66a942a9ab50fb0adfd2634bf9138fcc9
Katana Nevermore <128977921+NevermoreKatana@users.noreply.github.com>
2023-04-25 19:58:18 +0300

    Add files via upload

---
+++ Rapid exponentiation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+static BigInteger PowMod(BigInteger x,BigInteger y, BigInteger n)
+{
+    BigInteger r = 1;
+    while (y != 0)
+    {
+        if ((y & 0x01) == 1)
+        {
+            r = (r * x) % n;
+        }
+        x = (x * x) % n;
+        y >>= 1;
+    }
+    return r;
+}
+
+static void Main()
+{
+    BigInteger result = PowMod(2, 25, 81613123);
+    Console.WriteLine($"{result}");
+}
+Main();
